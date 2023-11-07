0d5e1290bd3b86633a1bbd7b44c692f4761181cc
perpetualgrimace <hello@jamesferrell.me>
2023-11-06 19:22:50 -0500

    adds social icons

---
+++ components/icons/social/BandcampIcon.svg.jsx
@@ -0,0 +1,13 @@
+export default function BandcampIcon() {
+  return (
+    <svg
+      className="bandcamp-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+    >
+      <path fill="#E4D9D7" d="M8 25.809l7.437-13.103H32l-7.438 13.103z" />
+    </svg>
+  );
+}
