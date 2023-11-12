16bc68d3c3bb69f163a1df550ffc482e89777dec
perpetualgrimace <hello@jamesferrell.me>
2023-11-12 01:06:50 -0500

    displays all stats in StatsSection (TODO: style)

--- components/layout/components/StatTile.jsx
+++ components/layout/components/StatTile.jsx
@@ -1,10 +1,15 @@
+import RetinaImg from "/components/common/RetinaImg";
+
 export default function StatTile(props) {
-  const { title, subhead, children } = props;
+  const { title, subhead, imgClass, imgSrc, children } = props;
 
   return (
     <section className="stat-tile">
       <h3>{title}</h3>
       {subhead && <p className="stat-tile-subhead">{subhead}</p>}
+
+      <RetinaImg className={imgClass} file={imgSrc} extension="jpg" />
+
       {children}
     </section>
   );
--- components/layout/components/StatsSection.jsx
+++ components/layout/components/StatsSection.jsx
@@ -1,8 +1,16 @@
 import Stats from "/data/stats.json";
 
+import loadIcon from "/helpers/loadIcon";
+
 import StatTile from "/components/layout/components/StatTile";
 
-const { neptune, decayedRemade } = Stats;
+const { neptune, decayedRemade, followers } = Stats;
+const {
+  spotifyListeners,
+  twitterFollowers,
+  facebookFollowers,
+  instagramFollowers,
+} = followers;
 
 export default function StatsSection() {
   return (
@@ -14,6 +22,8 @@ export default function StatsSection() {
       <StatTile
         title="Neptune EP"
         subhead={makeReleaseDate(neptune.releaseDate)}
+        imgSrc="artwork/neptune"
+        imgClass="stat-tile-album-img stat-tile-img"
       >
         {makeAlbumTile(neptune)}
       </StatTile>
@@ -21,8 +31,31 @@ export default function StatsSection() {
       <StatTile
         title="Decayed | Remade"
         subhead={makeReleaseDate(decayedRemade.releaseDate)}
+        imgSrc="artwork/decayed-remade"
+        imgClass="stat-tile-album-img stat-tile-img"
       >
         {makeAlbumTile(decayedRemade)}
+      </StatTile>
+
+      <StatTile
+        title="Media statistics"
+        imgSrc="thumbnail/epk-2"
+        imgClass="stat-tile-img"
+      >
+        <ul className="stat-list">
+          <li className="stat">
+            {loadIcon("Spotify")} {spotifyListeners} monthly listeners
+          </li>
+          <li className="stat">
+            {loadIcon("Twitter")} {twitterFollowers} followers
+          </li>
+          <li className="stat">
+            {loadIcon("Facebook")} {facebookFollowers} followers
+          </li>
+          <li className="stat">
+            {loadIcon("Instagram")} {instagramFollowers} followers
+          </li>
+        </ul>
       </StatTile>
     </section>
   );
@@ -37,5 +70,20 @@ function makeReleaseDate(releaseDate) {
 }
 
 function makeAlbumTile(statObj) {
-  return <div className="stat-tile-album">{statObj.spotifyPlays}</div>;
+  const { spotifyPlays, bandcampSales, topTrack } = statObj;
+  return (
+    <ul className="stat-list">
+      <li className="stat">
+        {loadIcon("Spotify")} {spotifyPlays} plays
+      </li>
+      <li className="stat">
+        {loadIcon("Bandcamp")} {bandcampSales} sales
+      </li>
+      <li className="stat">
+        Top track <br />
+        {topTrack.title}
+        {topTrack.spotifyPlays} plays
+      </li>
+    </ul>
+  );
 }
